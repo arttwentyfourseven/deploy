8045e0337af9f20b1e1625091313d7746c98abb4
Art <58167052+arttwentyfourseven@users.noreply.github.com>
2025-07-30 13:55:21 +1000

    Update setup_dydx.sh
    Point to dev repo

--- setup_dydx.sh
+++ setup_dydx.sh
@@ -2,7 +2,7 @@
 
 # Pulling the required Docker images
 docker compose pull
-docker pull hummingbot/hummingbot:latest_dydx
+docker pull arttwentyfourseven/hummingbot:latest_dydx
 
 # Creating .env file with the required environment variables
 echo "CONFIG_PASSWORD=a" > .env
